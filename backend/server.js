b618f451164876bc80be387b3f21ed5b685d720c
Ashkin37 <roykasi52@gmail.com>
2020-10-27 11:37:47 +0300

    added models for mongo database configuration

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,13 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import products from './data/products';
+
+dotenv.config()
+
+const app = express 
+
+
 const express = require('express')
 const app = express()
 app.listen(5000, console.log('server running on port 5000'))
