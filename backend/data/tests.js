4408e826003c8a47ddb97c4a9818f9e8d9d24336
Ashkin37 <roykasi52@gmail.com>
2020-10-27 11:48:42 +0300

    mock data api for tests created

---
+++ backend/data/tests.js
@@ -0,0 +1,76 @@
+const tests = [
+  {
+    name: 'diabetes',
+    image: '/images/diabetes.jpg',
+    description:
+      'diabetes',
+    brand: 'diabetes',
+    category: 'Lab Test',
+    price: 89.99,
+    countInStock: 3,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'red blood cells',
+    image: '/images/rbc.jpg',
+    description:
+      'red blood cell count test',
+    brand: 'red blood cells ',
+    category: 'Lab Test',
+    price: 599.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'gluten ',
+    image: '/images/gluten.jpg',
+    description:
+      'gluten test',
+    brand: 'gluten',
+    category: 'Lab Test',
+    price: 929.99,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'HIV',
+    image: '/images/hiv.jpg',
+    description:
+      'HIV TEST',
+    brand: 'HIV',
+    category: 'Lab Test',
+    price: 399.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'drug test',
+    image: '/images/drug.jpg',
+    description:
+      'drug test',
+    brand: 'drugs',
+    category: 'Lab Test',
+    price: 49.99,
+    countInStock: 7,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'pregnancy',
+    image: '/images/pregnancy.jpg',
+    description:
+      'pregnancy',
+    brand: 'pregnancy',
+    category: '',
+    price: 29.99,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+]
+
+export default pLab Tests
