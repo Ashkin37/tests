b041e58f9fa058992f9451c135f20543108a696b
Ashkin37 <roykasi52@gmail.com>
2020-10-28 15:41:50 +0300

    updated footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import { Navbar, Nav } from 'react-bootstrap'
+
 
 const Footer = () => {
-	return (
-		<footer>
-			<Container>
-			 <Row>
-			  <Col className='text-center' py-3> Copyright tests </Col>
-			 </Row>
-			</Container>  
-		</footer>
-
-		)
+  return (
+    <footer>
+       <Navbar bg='success' variant='dark' expand='lg' collapseOnSelect>
+      <Container>
+        <Row>
+          <Col className='text-center py-3'> DON'T SELL MY PERSONAL INFORMATION </Col>
+        </Row>
+      </Container>
+      </Navbar>
+    </footer>
+  )
 }
 
 export default Footer
